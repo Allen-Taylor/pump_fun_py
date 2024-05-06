e1904c2ba34a6628a6301a2d4fe756d4aca92325
AL THE BOT FATHER <98119917+Allen-Taylor@users.noreply.github.com>
2024-05-06 14:14:34 +0200

    Update config.py

--- pump_fun_py/config.py
+++ pump_fun_py/config.py
@@ -2,7 +2,7 @@ from solana.rpc.api import Client
 from solders.keypair import Keypair #type: ignore
 
 PUB_KEY = ""
-PRIV_KEY = ""
+PRIV_KEY = "" # BASE58 STRING FORMAT
 RPC = ""
 client = Client(RPC)
 payer_keypair = Keypair.from_base58_string(PRIV_KEY)
