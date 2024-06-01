19fa237d0c9d839a027baf62ef20bb9657b474c7
AL THE BOT FATHER <98119917+Allen-Taylor@users.noreply.github.com>
2024-06-01 12:51:44 +0200

    Update utils.py

--- pump_fun_py/utils.py
+++ pump_fun_py/utils.py
@@ -44,25 +44,8 @@ def get_token_balance(base_mint: str):
         return None
 
 def get_coin_data(mint_str):
-    url = f"https://client-api-2-74b1891ee9f9.herokuapp.com/coins/{mint_str}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
-        "Accept": "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Referer": "https://www.pump.fun/",
-        "Origin": "https://www.pump.fun",
-        "Connection": "keep-alive",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "cross-site",
-        "If-None-Match": 'W/"43a-tWaCcS4XujSi30IFlxDCJYxkMKg"'
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    # END POINT IS DEAD
+    return None
 
 def confirm_txn(txn_sig, max_retries=20, retry_interval=3):
     retries = 0
