afd76b8160af5f01e4ccfcd4ee55c80f6a59804f
AL THE BOT FATHER <98119917+Allen-Taylor@users.noreply.github.com>
2024-06-07 21:48:08 +0200

    Update utils.py

--- pump_fun_py/utils.py
+++ pump_fun_py/utils.py
@@ -44,24 +44,27 @@ def get_token_balance(base_mint: str):
         return None
 
 def get_coin_data(mint_str):
-    url = f"https://client-api-2-74b1891ee9f9.herokuapp.com/coins/{mint_str}"
+    url = f"https://frontend-api.pump.fun/coins/{mint_str}"
+
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
         "Accept": "*/*",
         "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Referer": "https://www.pump.fun/",
-        "Origin": "https://www.pump.fun",
-        "Connection": "keep-alive",
         "Sec-Fetch-Dest": "empty",
         "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "cross-site",
-        "If-None-Match": 'W/"43a-tWaCcS4XujSi30IFlxDCJYxkMKg"'
+        "Sec-Fetch-Site": "same-site",
+        "If-None-Match": 'W/"41b-5sP6oeDs1tG//az0nj9tRYbL22A"',
+        "Priority": "u=4"
     }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+    except Exception as e:
         return None
 
 def confirm_txn(txn_sig, max_retries=20, retry_interval=3):
