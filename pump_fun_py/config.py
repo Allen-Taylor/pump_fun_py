70fd0fa0860210361ef210fd7af29b562f8663b1
AL THE BOT FATHER <98119917+Allen-Taylor@users.noreply.github.com>
2024-08-24 20:39:48 +0200

    Delete pump_fun_py directory

--- pump_fun_py/config.py
+++
@@ -1,8 +0,0 @@
-from solana.rpc.api import Client
-from solders.keypair import Keypair #type: ignore
-
-PUB_KEY = "" # WALLET ADDRESS
-PRIV_KEY = "" # BASE58 STRING FORMAT
-RPC = "https://api.mainnet-beta.solana.com" # Use Helius or Quicknode for better performance
-client = Client(RPC)
-payer_keypair = Keypair.from_base58_string(PRIV_KEY)
