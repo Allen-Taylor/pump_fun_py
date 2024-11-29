35c03e8647ac166d5df310340affee70608efd22
AL THE BOT FATHER <98119917+AL-THE-BOT-FATHER@users.noreply.github.com>
2024-11-29 22:42:00 +0100

    Add files via upload

---
+++ pump_fun_py/coin_data.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+from typing import Optional
+from construct import Flag, Int64ul, Padding, Struct
+from solders.pubkey import Pubkey  # type: ignore
+from spl.token.instructions import get_associated_token_address
+from config import client
+from constants import PUMP_FUN_PROGRAM
+
+@dataclass
+class CoinData:
+    mint: Pubkey
+    bonding_curve: Pubkey
+    associated_bonding_curve: Pubkey
+    virtual_token_reserves: int
+    virtual_sol_reserves: int
+    token_total_supply: int
+    complete: bool
+
+def get_virtual_reserves(bonding_curve: Pubkey):
+    bonding_curve_struct = Struct(
+        Padding(8),
+        "virtualTokenReserves" / Int64ul,
+        "virtualSolReserves" / Int64ul,
+        "realTokenReserves" / Int64ul,
+        "realSolReserves" / Int64ul,
+        "tokenTotalSupply" / Int64ul,
+        "complete" / Flag
+    )
+    
+    try:
+        account_info = client.get_account_info(bonding_curve)
+        data = account_info.value.data
+        parsed_data = bonding_curve_struct.parse(data)
+        return parsed_data
+    except Exception:
+        return None
+
+def derive_bonding_curve_accounts(mint_str: str):
+    try:
+        mint = Pubkey.from_string(mint_str)
+        bonding_curve, _ = Pubkey.find_program_address(
+            ["bonding-curve".encode(), bytes(mint)],
+            PUMP_FUN_PROGRAM
+        )
+        associated_bonding_curve = get_associated_token_address(bonding_curve, mint)
+        return bonding_curve, associated_bonding_curve
+    except Exception:
+        return None, None
+
+def get_coin_data(mint_str: str) -> Optional[CoinData]:
+    bonding_curve, associated_bonding_curve = derive_bonding_curve_accounts(mint_str)
+    if bonding_curve is None or associated_bonding_curve is None:
+        return None
+
+    virtual_reserves = get_virtual_reserves(bonding_curve)
+    if virtual_reserves is None:
+        return None
+
+    try:
+        return CoinData(
+            mint=Pubkey.from_string(mint_str),
+            bonding_curve=bonding_curve,
+            associated_bonding_curve=associated_bonding_curve,
+            virtual_token_reserves=int(virtual_reserves.virtualTokenReserves),
+            virtual_sol_reserves=int(virtual_reserves.virtualSolReserves),
+            token_total_supply=int(virtual_reserves.tokenTotalSupply),
+            complete=bool(virtual_reserves.complete),
+        )
+    except Exception as e:
+        print(e)
+        return None
---
+++ pump_fun_py/example_sell.py
@@ -0,0 +1,7 @@
+from pump_fun import sell
+
+# Sell Example
+mint_str = "pump_token_address"
+percentage = 100
+slippage = 25
+sell(mint_str, percentage, slippage)
