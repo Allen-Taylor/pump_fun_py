39204586db28c303fec8256ce30775c47a599f51
AL THE BOT FATHER <98119917+AL-THE-BOT-FATHER@users.noreply.github.com>
2024-12-08 19:58:16 +0100

    Delete pump_fun_py directory

--- pump_fun_py/config.py
+++
@@ -1,9 +0,0 @@
-from solana.rpc.api import Client
-from solders.keypair import Keypair #type: ignore
-
-PRIV_KEY = "base58_priv_str_here"
-RPC = "rpc_url_here"
-UNIT_BUDGET =  100_000
-UNIT_PRICE =  1_000_000
-client = Client(RPC)
-payer_keypair = Keypair.from_base58_string(PRIV_KEY)
